b2c9248031caf6a3789d6fa8bb6c811cfde10f95
iguissouma <issam.guissouma@laposte.net>
2015-09-20 20:47:34 +0200

    increment version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 // package metadata file for Meteor.js
 var packageName = 'iguissouma:ionic-material-design-lite';
 var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
-var version = '1.0.0-rc-1';
+var version = '1.0.0-rc-1-1';
 var summary = 'Material Design (lite) style override for Ionic Framework.';
 var gitLink = 'https://github.com/iguissouma/ionic-material-design-lite';
 var documentationFile = 'README.md';
